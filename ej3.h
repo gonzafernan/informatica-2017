c70dac6fb92678be622d9f74f6f0053fa580f058
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:45:13 -0300

    Update ej3.h

--- ej3.h
+++ ej3.h
@@ -2,9 +2,6 @@
 //  ej3.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 8/30/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef ej3_h
 #define ej3_h
