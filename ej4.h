e502c0097c94fd6b023cf8df0c7a385338ebb426
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:45:42 -0300

    Update ej4.h

--- ej4.h
+++ ej4.h
@@ -2,9 +2,6 @@
 //  ej4.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 8/30/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef ej4_h
 #define ej4_h
