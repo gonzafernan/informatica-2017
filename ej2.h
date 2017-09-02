76df84de3973c71b7f7209d5667790bf3d938b78
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:44:42 -0300

    Update ej2.h

--- ej2.h
+++ ej2.h
@@ -2,9 +2,6 @@
 //  ej2.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 8/30/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef ej2_h
 #define ej2_h
