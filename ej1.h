e999206d6270b5fd8ee16f7323fa24a27a2cf776
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:43:27 -0300

    Update ej1.h

--- ej1.h
+++ ej1.h
@@ -2,9 +2,6 @@
 //  ej1.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 8/30/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef ej1_h
 #define ej1_h
