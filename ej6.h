83562a255d8772830bbfcf6f146bfd639a8f3d10
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:46:36 -0300

    Update ej6.h

--- ej6.h
+++ ej6.h
@@ -2,9 +2,6 @@
 //  ej6.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 9/2/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef ej6_h
 #define ej6_h
