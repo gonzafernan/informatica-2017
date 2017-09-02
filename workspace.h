4da6d3a8784067c4d0b8665fb3216a0affc64050
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-02 20:47:18 -0300

    Update workspace.h

--- workspace.h
+++ workspace.h
@@ -2,9 +2,6 @@
 //  workspace.h
 //  Informatica-2017
 //
-//  Created by Gonzalo Gabriel Fernández on 8/30/17.
-//  Copyright © 2017 Gonzalo Gabriel Fernández. All rights reserved.
-//
 
 #ifndef workspace_h
 #define workspace_h
