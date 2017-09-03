cf3c1bcff71f36bdb42925a7d92dde6049f3b0d5
giunta <gzsebas@hotmail.com>
2017-09-02 21:14:51 -0300

    main editado por seba
    es como lo tenia hecho yo el codigo del main, esta un poco mas interactivo de lo que estaba.

--- main.c
+++ main.c
@@ -12,14 +12,32 @@
 #include "ej6.h"
 #include "workspace.h"
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    //ej1();
-    //ej2();
-    //ej3();
-    //ej4();
-    //ej5();
-    ej6();
-    //workspace();
+   int ejercicio;
+   printf("ecribe el numero del ejercicio a ejecutarse\n");
+    scanf("%d",&ejercicio);
+    switch (ejercicio) {
+            case(1):
+              ej1();
+            break;
+            case(2):
+              ej2();
+            break;
+            case(3):
+              ej3();
+            break;
+            case(4):
+               ej4();
+            break;
+            case(5):
+              ej5();
+            break;
+            case(6):
+               ej6();
+            break;
+            //incluir desde aca todos los ejercicios siguientes
+        default:
+            printf("no existe ese ejercicio o no esta hecho")
+    }
+        
     return 0;
 }
