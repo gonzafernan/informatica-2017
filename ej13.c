dc56a1bc052789712726d97009f6b763c186b984
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-12 12:01:34 -0300

    Update ej13.c

--- ej13.c
+++ ej13.c
@@ -1,39 +1,62 @@
-/*
-Escriba un programa que
-a. Genere un vector con valore aleatorios enteros
-b. Recorra el vector y guarde el promedio de los valores en una variable
-c. Encuentre el menor de los elementos
-*/
+//
+//  ej13.c
+//  Informatica-2017
+//  Escriba un programa que
+//  a. Genere un vector con valore aleatorios enteros
+//  b. Recorra el vector y guarde el promedio de los valores en una variable
+//  c. Encuentre el menor de los elementos
+//
+//
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int ej13(){
+    //Declaración de variables enteras
+    int largo,i;
+    printf("Ingrese cantidad de componentes del vector:\n");
+    scanf("%d",&largo);
+    
+    //Declaración de variables long
+    long vector[largo],menor;
    //srand:generador de semilla. Esto nos permite establecer el método de generación de los números random.
    //time: esta semilla utiliza el tiempo para establecer los random. Por esto 2 vectores generados consecutivamente no se repiten.
    //NULL: inicializa la función time en 0.
    //rand: permite asignar valores random.
-   srand(time(NULL));
-   int tam, i, j, sum=0, min;
-   float prom;
-   printf("Ingrese el tamaño del vector\n");
-   scanf("%d",&tam);
-   int V[tam];
-   //Asignación del vector random
-   printf("Vector Random\n");
-   for (i=1; i<=tam; i++){
-      V[(i-1)]=rand();
-      printf("Elemento %d = %d\n",i,V[(i-1)]);
-   } //Promedio y menor elemento
-   for (j=1; j<=tam; j++){
-      sum=sum+V[(j-1)];
-      if (j==1){
-         min=V[(j-1)];
-      } else if (V[j-1] < min){
-         min=V[(j-1)];
-      }
-   } prom=sum/tam;
-   printf("El promedio es igual a %f\nEl menor de los elementos es %d",prom,min);
-return 0;
+    srand(time(NULL)); //Para que en cada ejecucion del programa los valores aleatorios sean distintos
+    
+    //Bucle para cargar el vector con el largo asignado con numeros aleatorios
+    for (i=1;i<=largo;i++){
+        vector[i-1]=rand();
+    }
+    
+    //Bucle para calcular el promedio
+    float sum=0;
+    for (i=1;i<=largo;i++){
+        sum= vector[i-1]+sum;
+    }
+    float prom=sum/largo;
+    
+    //Bucle para buscar menor componente
+    menor=vector[0];
+    for (i=1;i<=largo;i++){
+        if(vector[i]<=menor){
+            menor=vector[i];
+        }
+    }
+    
+    //Resultados
+    printf("El vector de números aleatorios es: \n");
+    printf("[");
+    for (i=1;i<=largo;i++){
+        if (i==largo){
+            printf("%ld]\n",vector[i-1]);
+        } else {
+            printf("%ld, ",vector[i-1]);
+        }
+    }
+    printf("El promedio de las componentes del vector es: %f\n",prom);
+    printf("La menor de las componentes del vector es: %ld\n",menor);
+    return 0;
 }
