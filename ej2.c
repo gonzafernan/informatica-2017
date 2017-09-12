ba9d00fa0a225a180b713c9c8c953c6565b54ab6
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-12 11:37:30 -0300

    Update ej2.c

--- ej2.c
+++ ej2.c
@@ -13,39 +13,20 @@ int ej2(){
     //Bloque condicional switch
     switch (letra) {
         case 'a':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'e':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'i':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'o':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'u':
-            printf("%c es una vocal\n",letra);
-            break;
         //Bloque opcional para agregar vocales mayúsculas
         case 'A':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'E':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'I':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'O':
-            printf("%c es una vocal\n",letra);
-            break;
         case 'U':
             printf("%c es una vocal\n",letra);
             break;
         default:
             printf("El caracter no es una vocal\n");
-            break;
     }
     return 0;
 }
