6c7fb9d1159b6aa8ed1858aef6c6f707b5209762
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-12 02:41:33 -0300

    Update ej13.c
    Hice un par de cambios, vean si les parece bien. Esta bueno el código del Seba, solo cambie la forma en que encuentra la menor componente (es un poco más sencillo) y hice que el print del vector fuera un poco más bonito jajajjaa
    También arregle lo que te comente en el código Seba.

--- ej13.c
+++ ej13.c
@@ -1,52 +1,58 @@
 //
 //  ej13.c
 //  Informatica-2017
-//Escriba un programa que
-//a. Genere un vector con valore aleatorios enteros
-//b. Recorra el vector y guarde el promedio de los valores en una variable
-//c. Encuentre el menor de los elementos
+//  Escriba un programa que
+//  a. Genere un vector con valore aleatorios enteros
+//  b. Recorra el vector y guarde el promedio de los valores en una variable
+//  c. Encuentre el menor de los elementos
 //
+//
+
 #include <stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdlib.h>
+#include <time.h>
 
 int ej13(){
-  int largo,i,j,k,l;
-  printf("ingrese el largo del vector (un numero entero)\n");
-  scanf("%d",&largo);
-long vector[largo],menor;
-srand(time(NULL)); //para que en cada ejecucion del programa los valores aleatorios sean distintos
-//bucle para cargar el vector con el largo asignado con numeros aleatorios
-for (i=1;i<=largo;i++){
-  vector[i-1]=rand();
-}
-//bucle para calcular el promedio
-float sum=0;
-for (j=1;j<=largo;j++){
-  sum= vector[j-1]+sum;
-}
-float prom=sum/largo;
-//primer comparacion
-if (vector[0]<vector[1]){
-  menor = vector[0];
-}else{
-  menor=vector[1];
-}
-// bucle para ver cual es el numero menor
-
-for (k=2;k<largo;k++){
-  if (menor<vector[k]){
-    menor = menor;
-  }else{
-    menor=vector[k];
-  }
-}
-printf("el vector de numero aleatorios fue: \n");
-for (l=1;l<=largo;l++){
-  printf(" %d, ",vector[l-1]);
-}
-printf("\n");
-printf("el promedio de las componentes del vector fue: %f\n",prom);
-printf("la menor de las componentes del vector fue: %d\n",menor);
-return 0;
+    //Declaración de variables enteras
+    int largo,i;
+    printf("Ingrese cantidad de componentes del vector:\n");
+    scanf("%d",&largo);
+    
+    //Declaración de variables long
+    long vector[largo],menor;
+    srand(time(NULL)); //Para que en cada ejecucion del programa los valores aleatorios sean distintos
+    
+    //Bucle para cargar el vector con el largo asignado con numeros aleatorios
+    for (i=1;i<=largo;i++){
+        vector[i-1]=rand();
+    }
+    
+    //Bucle para calcular el promedio
+    float sum=0;
+    for (i=1;i<=largo;i++){
+        sum= vector[i-1]+sum;
+    }
+    float prom=sum/largo;
+    
+    //Bucle para buscar menor componente
+    menor=vector[0];
+    for (i=1;i<=largo;i++){
+        if(vector[i]<=menor){
+            menor=vector[i];
+        }
+    }
+    
+    //Resultados
+    printf("El vector de números aleatorios es: \n");
+    printf("[");
+    for (i=1;i<=largo;i++){
+        if (i==largo){
+            printf("%ld]\n",vector[i-1]);
+        } else {
+            printf("%ld, ",vector[i-1]);
+        }
+    }
+    printf("El promedio de las componentes del vector es: %f\n",prom);
+    printf("La menor de las componentes del vector es: %ld\n",menor);
+    return 0;
 }
