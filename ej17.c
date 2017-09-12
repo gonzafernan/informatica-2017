55d62bda0c9df127ef8617ccdbd42c0bf0452d74
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-12 12:35:51 -0300

    Update ej17.c

--- ej17.c
+++ ej17.c
@@ -16,15 +16,15 @@ int ej17(){
   };
 struct vectorR3 v1,v2,v3;
 //asignacion de las componentes de los vectores
-printf("ingrese las 3 componentes del vector 1 en orden \n");
+printf("Ingrese las 3 componentes del vector 1 en orden \n");
 scanf("%f %f %f",&v1.x,&v1.y,&v1.z);
-printf("ingrese las 3 componentes del vector 2 en orden \n");
+printf("Ingrese las 3 componentes del vector 2 en orden \n");
 scanf("%f %f %f",&v2.x,&v2.y,&v2.z);
 //calculo del producto vectorial con un vector v3 como resultado
 v3.x= (v1.y*v2.z)-(v1.z*v2.y);
 v3.y= (v1.z*v2.x)-(v1.x*v2.z);
 v3.z= (v1.x*v2.y)-(v1.y*v2.x);
-printf("el vector resultado del producto vectorial es: (%f,%f,%f)\n",v3.x,v3.y,v3.z);
+printf("El vector resultado del producto vectorial es: (%f,%f,%f)\n",v3.x,v3.y,v3.z);
   
 return 0;
 }
