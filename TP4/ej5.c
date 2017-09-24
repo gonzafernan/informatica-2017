489264865f6f033a10da86e963d691e426c565d0
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-24 19:58:27 -0300

    Ejercicio 5
    Otrooooooo

---
+++ TP4/ej5.c
@@ -0,0 +1,19 @@
+//
+//  ej5.c
+//  Informatica-2017
+//  Escriba una función que reciba un vector de N elementos (N variable)
+//  e imprima por pantalla el vector en un formato legible para el ser humano
+//  (ej: utilizando notación algebraica: “ (v1, v2, ..., vn) ”)
+//
+
+void ej5(double *u,num){
+  int i;
+  printf("Vector en notación algebraica:\n(");
+  for (i=0;i<num;i++){
+    if (i<num-1){
+      printf("%lf, ",u[i]);
+    } else {
+      printf("%lf)\n",u[i]);
+    }
+  }
+}
