cdf8147158365d508ffa95d1361ebeeae406bff7
Sebastian <gzsebas@hotmail.com>
2017-09-26 06:14:37 -0300

    Update ejercicios.h

--- TP4/ejercicios.h
+++ TP4/ejercicios.h
@@ -16,7 +16,7 @@ int ej6();
 int ej7();
 int ej8();
 int ej9();
-void ej10(double **matriz,double M,double N);
+double **ej10(double **matriz,double M,double N);
 int ej11();
 int ej12();
 int ej13();
