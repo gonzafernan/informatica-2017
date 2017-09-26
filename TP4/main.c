1638a84cb9aa7c4c1c2666e6b7863876e2381d41
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-26 10:42:26 -0300

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -1,9 +1,6 @@
-//
-//  main.c
-//  Informatica-2017
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "ejercicios.h"
 
 typedef int boolean;
@@ -11,58 +8,40 @@ typedef int boolean;
 #define FALSE 0
 boolean bandera=TRUE;
 int main() {
-   int ejercicio;
-
+   int seccion,num;
+   double s;
+   char resp;
+   //srand:generador de semilla. Esto nos permite establecer el método de generación de los números random.
+   //time: esta semilla utiliza el tiempo para establecer los random. Por esto 2 vectores generados consecutivamente no se repiten.
+   //NULL: inicializa la función time en 0.
+   srand(time(NULL)); //Para que en cada ejecucion del programa los valores aleatorios sean distintos
    do{
-      printf("Ingrese el número del ejercicio a ejecutar:\n");
-       scanf(" %d",&ejercicio);
-       switch (ejercicio) {
+      printf("Ingrese sección del trabajo práctico a ejecutar, siendo:\n  1- Ejercicio 6: Donde se reúnen los ejercicios del 1 al 5 en un solo procedimiento.\n  2- Ejercicio 13: Donde se reúnen los ejercicios del 7 al 12 en un solo procedimiento.\n  3- Sección sucesión de Fibonacci (RECURSIÓN).");
+       scanf(" %d",&seccion);
+       switch (seccion) {
                case(1):
-                 ej1();
+               //////////////////////////////////////////////////////////////////////////////
+               //              Sección para ejercicios del 1 al 6 (ARREGLOS 1D)
+               // Ejercicio 6
+               ej6();
+
                break;
                case(2):
-                 ej2();
+               //////////////////////////////////////////////////////////////////////////////
+               //Sección del trabajo práctico donde se trabaja con los ejercicios 7 al 13 (ARREGLOS 2D)
+               //(Se creo un ejercicio similar al ejercicio 6, pero con matrices)
+               ej13();
+
                break;
                case(3):
-                 ej3();
-               break;
-               case(4):
-                  ej4();
-               break;
-               case(5):
-                 ej5();
-               break;
-               case(6):
-                  ej6();
-               break;
-               case(7):
-                  ej7();
-               break;
-               case(8):
-                  ej8();
-               break;
-               case(9):
-                  ej9();
-               break;
-               case(10):
-                  ej10();
-               break;
-               case(11):
-                  ej11();
-               break;
-               case(12):
-                  ej12();
-               break;
-               case(13):
-                  ej13();
-               break;
-               case(14):
-                  ej14();
+               //////////////////////////////////////////////////////////////////////////////
+               //                Sección sucesión de Fibonacci (RECURSIÓN)
+                 ej14();
                break;
            default:
-               printf("No existe tal ejercicio o no esta hecho\n");
+               printf("ERROR: No existe tal sección.\n");
        } char resp;
-         printf("\nDesea ejecutar otro ejercicio?? Y/N\n");
+         printf("\nDesea continuar? Y/N\n");
          scanf(" %c",&resp);
       switch (resp){
          case('y'):
@@ -72,7 +51,6 @@ int main() {
             bandera=FALSE;
          }
 }   while(bandera);
-   printf("Donde esta Santiago Maldonado?");
+
 return 0;
 }
-
