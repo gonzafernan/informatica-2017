e74a2b88e92331657403a57796ef67bcca3cca83
GinoAvanzini <31598976+GinoAvanzini@users.noreply.github.com>
2017-09-25 21:23:24 -0300

    Update ej7.c
    En teoría está bien esta wea

--- TP4/ej7.c
+++ TP4/ej7.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escriba una función que reciba como argumento un par de entero M y N,
+cree una matriz de MxN de tipo double dinámicamente (calloc) y
+devuelva un apuntador con la direccón de memoria del arreglo creado*/
+
+int **ej7(int M, int N){
+  int i;
+  double **mat;
+  mat=calloc(M,sizeof(double*));
+  for (i=0;i<N;i++){
+    mat[i]=calloc(N,sizeof(double));
+  }
+  return mat;
+}
