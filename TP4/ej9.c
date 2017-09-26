021cefa0daf61d2ce6c62bd53f19d1692d9dd311
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-09-25 22:48:08 -0300

    Ejercicio 9
    Por favor, verifiquen que funciona!

---
+++ TP4/ej9.c
@@ -0,0 +1,17 @@
+//
+//  ej9.c
+//  Informatica-2017
+//  Repita el ejercicio (3) para el caso de una matriz de M x N elementos
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void ej9(double **matriz,double M,double N,double s){
+  int i,j;
+  for (i=0;i<M;i++){
+    for (j=0;j<N;j++){
+      matriz[i][j]=s*matriz[i][j];
+    }
+  }
+}
