e211d8e1c0ab9b429ebac7fa4f40201af8c9bfb0
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-10-17 12:23:41 -0300

    Create manejador_signals.c

---
+++ TP6/manejador_signals.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>			// Memset
+
+#include "headers.h"
+
+// Definición de tipo booleano.
+typedef int boolean;
+#define TRUE 1
+#define FALSE 0
+boolean en_ejecucion;
+
+// Variables globales utilizadas para la comunicación entre procesos.
+#define SERVER 0
+#define CLIENTE 1
+int rolProceso;
+
+//Esta funcion maneja las señales que recibe del kernel ("interrupciones de software")
+
+void manejador_signals(int signal_type){
+  char rolProceso_[10];
+  if(rolProceso == SERVER){
+	  strcpy(rolProceso_, "Servidor");
+  } else{
+	  strcpy(rolProceso_, "Cliente");
+  }
+  if (signal_type == SIGTERM){
+	  en_ejecucion = FALSE;
+	  printf("%s: Señal SIGTERM recibida. Terminando proceso.\n", rolProceso_);
+  }
+  else if (signal_type == SIGKILL){
+	  en_ejecucion = FALSE;
+	  printf("%s: Señal SIGKILL recibida. Terminando proceso.\n", rolProceso_);
+  }
+  else
+	printf("Señal desconocida. Ignorando...\n\n");
+}
