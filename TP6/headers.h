bbd32cf44a49e2ab959c06e82af85354c654f40c
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-10-22 19:10:34 -0300

    Update headers.h

--- TP6/headers.h
+++ TP6/headers.h
@@ -1,6 +1,15 @@
 #ifndef headers_h
 #define headers_h
 
+int main(void);
 void manejador_signals(int signal_type);
+void proceso_A();
+void proceso_B();
+
+void *thread_H1(int *en_ejecucion, int sd, void *direccionSocketCliente, void *longitudDireccionCliente);
+void *thread_H2(int *en_ejecucion, int sd_aceptado);
+void *thread_H3(void);
+
+#define MAP_BOOLEAN(X) X==TRUE?"TRUE":"FALSE"
 
 #endif
