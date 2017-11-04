1f6b295316fc20e4df43846f7ac33e4765e459a1
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-11-03 21:24:31 -0300

    Update threads.c

--- TP6/threads.c
+++ TP6/threads.c
@@ -12,107 +12,88 @@
 #include <sys/wait.h>
 #include <string.h>			// Memset
 
-typedef int boolean;
-#define TRUE 1
-#define FALSE 0
-boolean en_ejecucion;
-
-// Mutex para la sincronización de los hilos:
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condicion = PTHREAD_COND_INITIALIZER;
-
-// Struct para argumentos de los hilos:
-struct attr{
-  boolean arg1;
-  int arg2;
-};
-
 // Espacio de memoria compartido:
 char *file;
+long int sz_file;
 
-void *thread_H1(int *en_ejecucion, int sd, void *direccionSocketCliente, void *longitudDireccionCliente) {
+extern boolean en_ejecucion;
+
+void *thread_H1(int sd, void *direccionSocketCliente, void *longitudDireccionCliente) {
   // El hilo 1 se bloquea esperando conexiones:
-  while (*en_ejecucion == TRUE) {
-    printf("Thread 1...\n");
-    int sd_cliente2;
-    sd_cliente2 = accept(sd, direccionSocketCliente, longitudDireccionCliente);
-    //printf("%d\n", sd_cliente2);
-  }
-
+   while (en_ejecucion == TRUE) {
+     //int sd_cliente2;
+     //sd_cliente2 = accept(sd, direccionSocketCliente, longitudDireccionCliente);
+   }
+  // printf("Ejecución del primer hilo finalizada con éxito.\n");
   return NULL;
 }
 
-void *thread_H2(void * attr_H2){
+void *thread_H2(void * sd){
   // El hilo 2 recibe los datos que el proceso A envía en bloques de 10 bytes.
   // Coloca esos datos en una estructura de datos compartida.
-  //pthread_mutex_init(&mutext, NULL);
-  struct attr *args_H2 = (struct attr *)attr_H2;
-  boolean en_ejecucion = args_H2 -> arg1;
-  int sd_aceptado = args_H2 -> arg2;
-  while (en_ejecucion == TRUE) {
-    printf("Thread 2...\n");
-    char buffer_lectura[10];
-    long int sz_file;
-    //Ahora intentamos leer los bytes transferidos. El proceso se bloquea si no hay nada que leer
-    recv(sd_aceptado, &sz_file, sizeof(long int), 0);
-    printf("%lu\n", sz_file);
-    // Asignación dinámica de memoria:
-    file = malloc((sz_file)*sizeof(char));
+  int sd_aceptado = *(int *)sd;
+  char buffer_lectura[10];
+  //Ahora intentamos leer los bytes transferidos. El proceso se bloquea si no hay nada que leer
+  recv(sd_aceptado, &sz_file, sizeof(long int), 0);
+  //printf("Servidor:\n\tTamaño del archivo: %lu\n", sz_file);
+  // Asignación dinámica de memoria:
+  file = malloc((sz_file)*sizeof(char));
+  int i = 0, j = 0;
+  for (i = 0; i < sz_file; i++) {
+    file[i] = '\0';
+  }
 
-    // Recepción del archivo desde el cliente:
-    int i = 0, j = 0;
-    boolean band = TRUE;
-    while (band) {
-      //pthread_cond_wait(&condicion, &mutext);
-      recv(sd_aceptado, buffer_lectura, sizeof(buffer_lectura), 0);
-      //printf("%s\n", buffer_lectura);
-      //pthread_mutex_lock(&mutex);
-      for (i = 0; i < sizeof(buffer_lectura); i++) {
-        file[j * 10 + i] = buffer_lectura[i];
-        //printf("%c", buffer_lectura[i]);
-        if (buffer_lectura[i] == '\0') {
-          band = FALSE;
-          en_ejecucion = FALSE;
-          break;
-        }
-        printf("%c", buffer_lectura[i]);
+  // Recepción del archivo desde el cliente:
+  int pack=(sz_file/(int)sizeof(buffer_lectura))+1;
+  for (j = 0; j < pack; j++) {
+    recv(sd_aceptado, buffer_lectura, sizeof(buffer_lectura), 0);
+    for (i = 0; i < sizeof(buffer_lectura); i++) {
+      file[j * 10 + i] = buffer_lectura[i];
+      buffer_lectura[i] = '\0';
+      if (file[j * 10 + i] == sz_file) {
+        break;
       }
-      //pthread_mutex_unlock(&mutex);
-      j = j + 1;
     }
   }
-
-  free(file);
-  //pthread_mutex_destroy(&mutext);
-
+  do{sleep(1);}while(en_ejecucion == TRUE);
+  // printf("Ejecución del segundo hilo finalizada con éxito.\n");
   return NULL;
 }
 
-void *thread_H3(void *en_ejecucion){
+void *thread_H3(){
   // El hilo 3 muestra por pantalla lo que el hilo 2 va recibiendo.
-  while (*(int *)en_ejecucion == TRUE) {
-    int i = 0;
-    printf("Thread 3...\n");
-    while (file[i] != '\0') {
-      //pthread_cond_wait(&condicion, &mutext);
-      //pthread_mutex_lock(&mutext);
+  sleep(1);
+  int i = 0;
+  for (i = 0; i < sz_file; i++) {
+    if (file[i]!='\0' && (file + i) != NULL) {
       printf("%c", file[i]);
-      //pthread_mutex_unlock(&mutext);
-      i += 1;
+    } else {
+      i -= 1;
     }
   }
-
+  free(file);
+  do{sleep(1);}while(en_ejecucion == TRUE);
+  // printf("Ejecución del tercer hilo finalizada con éxito.\n");
   return NULL;
 }
 
-// void *thread_H4(char *file) {
-//   // El hilo 4 va guardando en un archivo lo que el hilo 2 va recibido.
-//   FILE *fp;
-//   fp = fopen("Archivo recibido.txt","w");
-//
-//   boolean band = TRUE;
-//   int i = 0;
-//   while (band) {
-//
-//   }
-// }
+ void *thread_H4() {
+
+   // El hilo 4 va guardando en un archivo lo que el hilo 2 va recibido.
+   sleep(1);
+   FILE *fp;
+   fp = fopen("Archivo recibido.txt","w");
+   int i;
+   for (i = 0; i < sz_file; i++) {
+     if (file[i]!='\0') {
+       fwrite((file + i),1,sizeof(char),fp);
+     } else {
+       i -= 1;
+     }
+   }
+   fclose(fp);
+   
+   do{sleep(1);}while(en_ejecucion == TRUE);
+  //  printf("Ejecución del cuarto hilo finalizada con éxito.\n");
+   return NULL;
+ }
