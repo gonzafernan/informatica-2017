afbbf3d7fb8b5e9cf4798f96553e0aa544c97891
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2017-11-03 21:20:06 -0300

    Update headers.h

--- TP6/headers.h
+++ TP6/headers.h
@@ -6,10 +6,24 @@ void manejador_signals(int signal_type);
 void proceso_A();
 void proceso_B();
 
-void *thread_H1(int *en_ejecucion, int sd, void *direccionSocketCliente, void *longitudDireccionCliente);
-void *thread_H2(void * attr);
-void *thread_H3(void *en_ejecucion);
+void *thread_H1(int sd, void *direccionSocketCliente, void *longitudDireccionCliente);
+void *thread_H2(void * sd);
+void *thread_H3();
+void *thread_H4();
 
+// Definición de tipo booleano.
+typedef int boolean;
+#define TRUE 1
+#define FALSE 0
+boolean en_ejecucion;
 #define MAP_BOOLEAN(X) X==TRUE?"TRUE":"FALSE"
+
+// Variables globales utilizadas para la comunicación entre procesos.
+#define SERVER 0
+#define CLIENTE 1
+int rolProceso;
+
+#define PUERTO_SERVER 3333
+#define MAX_LARGO_COLA 100
 
 #endif
