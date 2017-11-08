fa6e873ecfaed7ae29cdf925f45d4e4bc2c9f699
Sebastian <gzsebas@hotmail.com>
2017-11-08 02:18:18 -0300

    Update main.c

--- TP7/main.c
+++ TP7/main.c
@@ -34,17 +34,18 @@ int main(){
 
 
 
-  // int Ax,Ay,Bx,By;
-  // printf("\n ingrese en modo de coordernada (x,y) el punto inicial A (tomar en cuenta que el (0,0) es el extremo izquierdo de abajo):\n");
-  // scanf(" (%d,%d)",&Ax,&Ay);
-  // printf("ingrese en modo de coordernada (x,y) el punto final B :\n");
-  // scanf(" (%d,%d)",&Bx,&By);
-  // laberinto[9-Ay][Ax]=2;
-  // laberinto[9-By][Bx]=3;
-  /*for (i=0;i<10;i++){ //algoritmo para mostrar la matriz
+   int Ax,Ay,Bx,By;
+   printf("\n ingrese en modo de coordernada (x,y) el punto inicial A (tomar en cuenta que el (0,0) es el extremo izquierdo de abajo):\n");
+   scanf(" (%d,%d)",&Ax,&Ay);
+   printf("ingrese en modo de coordernada (x,y) el punto final B :\n");
+   scanf(" (%d,%d)",&Bx,&By);
+   laberinto[9-Ay][Ax]=2;
+   laberinto[9-By][Bx]=3;
+  
+  for (i=0;i<10;i++){ //algoritmo para mostrar la matriz
     for (j=0;j<10;j++){
         printf(" %d ",laberinto[i][j]);}
         printf("\n");}
-  */
+  
   return 0;
 }
